f715eb3b7cfb6db29dbfff284987184fdb4045ab
weirddog <a-sengupta@users.noreply.github.com>
2017-08-01 19:32:51 +0100

    pos_generator 2D update
    pos_generator.py has been updated to include a method writing a 2D array of
    atoms. Check the data dump folder for files dated 01-08 for examples. The latest
    one is the 'most correct' (as of this commit, it is thought to be completely
    correct as far as placing atoms is concerned). The .xyz file generated did not
    show anything happening though, CHECK THIS.
    
    The new 2D input file has also been included.
    
    Older 1D input and pos files had also been modified today, probably to change
    the type-1 type-2 relationship to a harmonic bond from LJ to check energy transf
    er is higher with harmonic bond.

--- Python_scripts/pos_generator.py
+++ Python_scripts/pos_generator.py
@@ -10,6 +10,7 @@ files for LAMMPS
 """
 
 from datetime import datetime
+import numpy as np
 
 def generate_pos(name=datetime.now().strftime('%d-%m_%H.%M')+".pos",wall_gap_multiplier=3,\
                  total_number_of_atoms=16,atomic_spacing=0.45,vacuum_gap_multiplier=3):
@@ -87,5 +88,115 @@ def generate_pos(name=datetime.now().strftime('%d-%m_%H.%M')+".pos",wall_gap_mul
     
     print("Position file has been written.")
     
-generate_pos()    
+    
+
+def write_atom_chain(f,starting_id,number_of_atoms,spacing,y_coord,z_coord,vacuum_gap_multiplier):#must use inside another func, f is undefined
+    
+    
+    ids = range(starting_id,starting_id+number_of_atoms)
+    x_multiples = np.arange(number_of_atoms)
+    half_row_number = int(number_of_atoms / 2)
+    y_coord_str = str(y_coord)
+    z_coord_str = str(z_coord)
+    
+    for i,j in zip(ids,x_multiples):
+        
+        id = str(i)
+        x_position = str( j*spacing )
+        yz_string = str("    %s    %s" %(y_coord_str,z_coord_str) )
+        
+        if i <= ids[half_row_number-1]:
+            
+            atom_type = str(1)
+            molecule_type = str(1)
+            f.write("%s %s %s %s %s \n" %(id,molecule_type,atom_type,x_position,yz_string))
+                  
+        else:
+            atom_type = str(2)
+            molecule_type = str(2)
+            x_position = str ( j*spacing + vacuum_gap_multiplier*spacing )
+            f.write("%s %s %s %s %s \n" %(id,molecule_type,atom_type,x_position,yz_string))   
+
+
+
+def generate_2D_pos(name=datetime.now().strftime('%d-%m_%H.%M')+"_2D.pos",wall_gap_multiplier=3,\
+                 row_atoms=16,rows=4,atomic_spacing=0.45,vacuum_gap_multiplier=3):
+    atoms = int(row_atoms*rows) 
+    total_bonds =  int(rows*(2*row_atoms -2) - row_atoms)
+    spacing = atomic_spacing
+    vacuum_gap = vacuum_gap_multiplier * spacing
+    x_lo = str(-1*wall_gap_multiplier*spacing)
+    x_hi = str( (row_atoms - 1)*spacing + vacuum_gap + wall_gap_multiplier*spacing )
+    y_lo = str( -1*wall_gap_multiplier*spacing)
+    y_hi = str( (rows-1)*spacing + wall_gap_multiplier*spacing )
+    z_lo = str( -0.00110000)
+    z_hi = str( 0.00110000)
+    
+    f = open(file=name,mode='x')
+    
+    f.write(str(row_atoms)+"x%sx1 (001) layers of simple cubic lattice \n \n" %(str(rows)) )
+    f.write(str(atoms)+" atoms \n"+str(total_bonds)+" bonds \n \n")
+    
+    f.write("2 atom types \n1 bond types \n \n")
+    
+    f.write(x_lo+" "+x_hi+" xlo xhi \n")
+    f.write(y_lo+" "+y_hi+" ylo yhi \n")
+    f.write(z_lo+" "+z_hi+" zlo zhi \n \n")
+
+    f.write("Atoms \n \n")
+    
+    atom_ids = range(1,atoms+1)
+    starting_ids_y_coords = zip(  atom_ids[::row_atoms], np.arange(rows) )
+    
+    for i,j in starting_ids_y_coords:
+        
+        write_atom_chain(f,i,row_atoms,atomic_spacing,j*atomic_spacing,0.00000,vacuum_gap_multiplier)
+        
+        
+        
+    f.write("\n")
+    f.write("Bonds \n \n")
+
+    
+    bond_type = "1"
+    half_number = int(row_atoms /2) 
+    no_bond_ids = []
+    for i in atom_ids:
+        no_bond_ids.append(i)
+    del no_bond_ids[half_number-1::half_number]
+    #no_bond_ids = no_bond_ids[:-1]
+    
+    counter = 1
+    for i in no_bond_ids: #this adds horizontal bonds
+        
+        source_atom = str(i)
+        target_atom = str(i+1)
+        f.write("%s %s %s %s \n" %(i,bond_type,source_atom,target_atom) )
+        counter +=1
+    
+    first_vertical_id = no_bond_ids[-1]+1
+    first_source = 1
+    first_target = first_source + row_atoms
+    while counter <= total_bonds: #this adds vertical bonds
+        counter +=1
+        f.write("%s %s %s %s \n" %(first_vertical_id,bond_type,first_source,first_target))
+        first_vertical_id +=1
+        first_source += 1
+        first_target += 1
+        
+
+        
+    f.write("\n")
+    
+    f.write("Bond Coeffs \n \n")
+    f.write("1 0.5 1.0 \n \n")
+    
+    f.write("Masses \n \n")
+    f.write("1 1.0 \n2 1.0")
+    
+    f.close()
+    
+    print("Position file has been written.")
+    
+generate_2D_pos()    
     
